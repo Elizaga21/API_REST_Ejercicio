d3e144e8d0452e889e6dde8fd39ffac9ce51d347
elisabetagullo <elisabetaudiovisual@gmail.com>
2023-04-03 09:36:21 +0200

    ClienteDao completado

--- src/main/java/com/ejercicio/dao/ClienteDao.java
+++ src/main/java/com/ejercicio/dao/ClienteDao.java
@@ -1,9 +1,35 @@
 package com.ejercicio.dao;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.ejercicio.entities.Cliente;
 
 public interface ClienteDao extends JpaRepository<Cliente, Long> {
+
+
+    @Query(value = "select c from Cliente c left join fetch c.hotel")
+    public List<Cliente> findAll(Sort sort);
+
+
+    /**
+     * El siguiente método recupera una página de cliente
+     */
+
+     @Query(value = "select c from Cliente c left join fetch c.hotel", countQuery = "select count(c) from Cliente c left join c.hotel")
+     public Page<Cliente> findAll(Pageable pageable);
+
+
+     /**
+      * El método siguiente recupera un cliente por el id
+      */
+
+      @Query(value = "select c from Cliente c left join fetch c.hotel where c.id = :id")
+      public Cliente findById(long id);
     
 }
